5f684f9de8c8196f8f09cba77951670d981c9765
mmoghal <mmoghal29@gmail.com>
2023-06-04 13:31:31 -0400

    Updated connection.js with CLEARDB_DATABASE_URL

--- config/connection.js
+++ config/connection.js
@@ -5,8 +5,8 @@ require('dotenv').config();
 const Sequelize = require('sequelize');
 
 // Create a new Sequelize instance based on the environment variables
-const sequelize = process.env.JAWSDB_URL
-  ? new Sequelize(process.env.JAWSDB_URL) // If JAWSDB_URL environment variable is present, use it for the connection
+const sequelize = process.env.CLEARDB_DATABASE_URL
+  ? new Sequelize(process.env.CLEARDB_DATABASE_URL) // If CLEARDB_DATABASE_URL environment variable is present, use it for the connection
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
       host: process.env.DB_HOST || 'localhost', // Use DB_HOST environment variable if present, otherwise use 'localhost'
       port: process.env.DB_PORT || 3306, // Use DB_PORT environment variable if present, otherwise use 3306
