70b18e749ad7780ce02d96bfb6900bb6e6781327
mmoghal <mmoghal29@gmail.com>
2023-06-03 21:19:36 -0400

    Added code for post-routes.js

---
+++ controllers/api/post-routes.js
@@ -0,0 +1,55 @@
+const postRouter = require("express").Router();
+const { Post, Comment, User } = require("../../models/");
+const withAuth = require("../../utils/auth");
+
+postRouter.post("/", withAuth, (req, res) => {
+  const body = req.body;
+  console.log(req.session.userId);
+  Post.create({ ...body, userId: req.session.userId })
+    .then(newPost => {
+      res.json(newPost);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+});
+
+postRouter.put("/:id", withAuth, (req, res) => {
+  console.log(req.body, req.params.id)
+  Post.update(req.body, {
+    where: {
+      id: req.params.id
+    }
+  })
+    .then(affectedRows => {
+      if (affectedRows > 0) {
+        res.status(200).end();
+      } else {
+        res.status(404).end();
+      }
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+});
+
+postRouter.delete("/:id", withAuth, (req, res) => {
+  console.log(req.body, req.params.id)
+  Post.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+    .then(affectedRows => {
+      if (affectedRows > 0) {
+        res.status(200).end();
+      } else {
+        res.status(404).end();
+      }
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+});
+
+module.exports = postRouter;
