1874cd77f4864e9376dc98d68e16b4a3f1b0ec57
mmoghal <mmoghal29@gmail.com>
2023-06-03 21:18:03 -0400

    Created schema Added code for comment-routes.js

---
+++ controllers/api/comment-routes.js
@@ -0,0 +1,15 @@
+const commentRouter = require("express").Router();
+const { Comment } = require("../../models/");
+const withAuth = require("../../utils/auth");
+
+commentRouter.post("/", withAuth, (req, res) => {
+  Comment.create({ ...req.body, userId: req.session.userId })
+    .then(newComment => {
+      res.json(newComment);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+});
+
+module.exports = commentRouter;
